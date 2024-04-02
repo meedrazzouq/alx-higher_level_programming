6a0fd19c8cda78ed894aadce60a35c288d1b8678
meedrazzouq <mohamedrzaaouq3110@gmail.com>
2024-04-02 16:47:12 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_arg(argv):
+    lenarg = len(argv) - 1
+    if lenarg == 0:
+        print("{:d}".format(lenarg))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= lenarg:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
