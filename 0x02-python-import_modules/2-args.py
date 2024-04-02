d974b065a293d5abba47d492bb8d6905ad64ede4
meedrazzouq <mohamedrzaaouq3110@gmail.com>
2024-04-02 15:59:15 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 import sys
-if len(sys.argv) == 0:
-    print("{} argument.".format(len(sys.argv)))
-elif len(sys.argv) == 1:
-    print("{} argument:".format(len(sys.argv)))
-    print("1:", sys.argv[0])
-elif len(sys.argv) > 1:
-    print("{} arguments:".format(len(sys.argv)))
-    for i, arg in enumerate(sys.argv[0:], start=1):
-        print("{}:".format(i), arg)
+
+num_args = len(sys.argv) - 1  # Number of arguments excluding the script name
+
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+    print("1:", sys.argv[1])
+else:
+    print(f"{num_args} arguments:")
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
