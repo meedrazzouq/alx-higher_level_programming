ea47cb5a58e443a4cf3ac80d5ef9e1cfd88bd1a8
meedrazzouq <mohamedrzaaouq3110@gmail.com>
2024-04-22 16:12:23 -0700

    Multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
+        return 0, None
     length = len(sentence)
     first_char = sentence[0]
     return length, first_char
