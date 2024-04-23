72bd214fca775d7f215f7408ca7d262a2fa36952
meedrazzouq <mohamedrzaaouq3110@gmail.com>
2024-04-23 05:56:34 -0700

    Find the max value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) <= 0:
         return None
-    max_value = 0
+    max_value = my_list[0]
     for element in my_list:
         if element > max_value:
             max_value = element
